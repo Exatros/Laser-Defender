0016db42c7f9f26b4a3f4de12e9f01d51a790826
Exatros <exatros@gmail.com>
2019-10-07 11:57:36 +0200

    #7 Spawn multiple enemies
    -Create another path
    -Add EnemySpawnder.cs
    -Add WaveConfig.cs
    -Create script to manages waves
    -Create method to spawn multiple enemies

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
     [SerializeField] float moveSpeed = 2f;
+
+    List<Transform> waypoints;
     int waypointIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
+        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
     }
 
