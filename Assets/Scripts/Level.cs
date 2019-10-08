e8d6483403a24856a09db72c868cb5152959216c
Exatros <exatros@gmail.com>
2019-10-08 12:12:29 +0200

    #16 Implement scene and delay loading scene
    -Created start and game over menu scene
    -Created UI with buttons for both scenes
    -Added level script to management scenes
    - Added delay after our death for finish all effects

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 2;
+
+    public void LoadGameOver()
+    {
+
+        StartCoroutine(WaitAndLoad());
+    }
+    
+    private IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene("Game Over");
+
+    }
+
+    public void LoadStage()
+    {
+        SceneManager.LoadScene("Stage 1");
+    }
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -117,7 +117,9 @@ public class Player : MonoBehaviour
 
     private void Die()
     {
+        FindObjectOfType<Level>().LoadGameOver();
         Destroy(gameObject);
         AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathSFXVolume);
+
     }
 }
