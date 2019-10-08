86fc39b99d808ebcf7c55f7f5f587a3e37c039af
Exatros <exatros@gmail.com>
2019-10-08 14:52:11 +0200

    #20 Spiner bomb
    -Added script which spin bomb

---
+++ Assets/Scripts/Spiner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spiner : MonoBehaviour
+{
+    [SerializeField] float speedOfSpin = 360f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,speedOfSpin * Time.deltaTime);
+    }
+}
