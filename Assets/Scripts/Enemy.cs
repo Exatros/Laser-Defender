f3ae8c77ff169c54a42aa650a201f445c630431a
Exatros <exatros@gmail.com>
2019-10-11 11:01:43 +0200

    #23 Change values
    -Changed enemies values
    -Changed waves
    -Changed projectiles dmg
    -Added special attack in enemy 5

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,19 +13,27 @@ public class Enemy : MonoBehaviour
     [SerializeField] float maxTimeBetweenShots = 1f;
     [SerializeField] GameObject projectile;
     [SerializeField] float projectileSpeed = 10f;
+    [Header("Special Attack")]
+    [SerializeField] bool specialAttack = false;
+    [SerializeField] float minReloadTimeForSpecialAttack = 2f;
+    [SerializeField] float maxReloadTimeForSpecialAttack = 2f;
+    [SerializeField] int numberOfSpecialProjetiles = 10;
+    float currentSpecialProjetiles =0 ;
     [Header("VFX effects")]
     [SerializeField] GameObject deathVFX;
     [SerializeField] float durationOfExplosion = 1f;
     [Header("Audio")]
     [SerializeField] AudioClip deathSFX;
     [SerializeField] AudioClip shootSFX;
-    [SerializeField] [Range(0,1)] float deathSFXVolume = 0.7f;
+    [SerializeField] [Range(0, 1)] float deathSFXVolume = 0.7f;
     [SerializeField] [Range(0, 1)] float shootSFXVolume = 0.25f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        currentSpecialProjetiles = numberOfSpecialProjetiles;
     }
 
     // Update is called once per frame
@@ -36,11 +44,27 @@ public class Enemy : MonoBehaviour
 
     private void CountDownAndShoot()
     {
-        shotCounter -= Time.deltaTime;
-        if(shotCounter <= 0)
+        shotCounter -= Time.fixedDeltaTime;
+        if (shotCounter <= 0)
         {
             Fire();
-            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+            if (!specialAttack)
+            {
+                shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+            }
+            else
+            {
+                if (currentSpecialProjetiles > 0)
+                {
+                    shotCounter = 0.15f;
+                    currentSpecialProjetiles--;
+                }
+                else
+                {
+                    shotCounter = Random.Range(minReloadTimeForSpecialAttack, maxReloadTimeForSpecialAttack);
+                    currentSpecialProjetiles = numberOfSpecialProjetiles;
+                }
+            }
 
         }
     }
@@ -55,13 +79,16 @@ public class Enemy : MonoBehaviour
         laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
         AudioSource.PlayClipAtPoint(shootSFX, Camera.main.transform.position, shootSFXVolume);
 
-
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
-        if (!damageDealer) { return; }
+        if (!damageDealer)
+        {
+            DeathVFX();
+            return;
+        }
         ProcessHit(damageDealer);
     }
 
@@ -80,8 +107,13 @@ public class Enemy : MonoBehaviour
         FindObjectOfType<GameSession>().AddToScore(scoreValue);
         FindObjectOfType<GameSession>().DecraseEnemyCounter();
         Destroy(gameObject);
+        DeathVFX();
+        AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathSFXVolume);
+    }
+
+    private void DeathVFX()
+    {
         GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
-        Destroy(explosion, durationOfExplosion);
-        AudioSource.PlayClipAtPoint(deathSFX,Camera.main.transform.position, deathSFXVolume );
+        Destroy(explosion, durationOfExplosion * explosion.GetComponent<ParticleSystem>().main.duration);
     }
 }
